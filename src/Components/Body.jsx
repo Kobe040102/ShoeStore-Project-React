dc2e56201eb74cc74982fd6508dce325f383eaee
Kobe040102 <34294040+Kobe040102@users.noreply.github.com>
2022-12-19 21:11:35 -0800

    Collection edit

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,4 +1,7 @@
 import "../Style/BodyStyle.css"
+import Women from "../Images/Women-Shoes.png"
+import Men from "../Images/Men-Shoe.jpg"
+import Kids from "../Images/Kids-Shoes.jpg"
 function Body() {
     return(
         <>
@@ -10,8 +13,13 @@ function Body() {
         <div className="Bod">
         <h1 className="Heading">New Collections</h1>
         </div>
-        <div className="Collection">
-            
+        <div className="Collec-pic">
+            <img src={Women} alt="Womens"/>
+            <div className="H1"><h1>Womens Collection</h1></div>
+            <img src={Men} alt="Men"/>
+            <div className="H2"><h1>Mens Collection</h1></div>
+            <img src={Kids} alt="Kids"/>
+            <div className="H3"><h1>Kids Collection</h1></div>
         </div>
         
         </>

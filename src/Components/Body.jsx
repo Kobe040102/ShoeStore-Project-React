92613550d320c02b16a5bd3fcef4e8f660e362ea
Kobe040102 <34294040+Kobe040102@users.noreply.github.com>
2022-12-20 23:20:47 -0800

    Progress

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -11,17 +11,25 @@ function Body() {
         <h1>Shoe<span className="Fi">Fit</span>Market</h1>
         </div>
         <div className="Bod">
-        <h1 className="Heading">New Collections</h1>
+        <h1 className="Heading">Our Products</h1>
         </div>
-        <div className="Collec-pic">
-            <img src={Women} alt="Womens"/>
-            <div className="H1"><h1>Womens Collection</h1></div>
-            <img src={Men} alt="Men"/>
-            <div className="H2"><h1>Mens Collection</h1></div>
-            <img src={Kids} alt="Kids"/>
-            <div className="H3"><h1>Kids Collection</h1></div>
-        </div>
+        <div className="Collec-pic"> 
         
+            <img src={Women} alt="Womens"/>
+            
+            <img src={Men} alt="Men"/>
+            
+            <img src={Kids} alt="Kids"/>
+            <div className="Collec-Ti">
+            <h1>Womens Collection</h1>
+            <h1>Mens Collection</h1>
+            <h1>Kids Collection</h1>
+            </div>
+        </div>
+        <div className="Sales">
+            <h1>On Sale</h1>
+            
+        </div>
         </>
 
     );
